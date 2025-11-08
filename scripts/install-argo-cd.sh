4b367dde993c27b65d5ca7d7e7d4a8550dc7b20e
quocanuit <quocanuit@gmail.com>
2025-11-08 17:13:10 +0700

    update

--- scripts/install-argo-cd.sh
+++ scripts/install-argo-cd.sh
@@ -18,4 +18,16 @@ helm upgrade --install argo-cd argo/argo-cd \
 
 echo "ArgoCD installed!"
 
+echo "Waiting for ArgoCD server to be ready..."
+kubectl wait --for=condition=available --timeout=300s deployment/argo-cd-argocd-server -n $NAMESPACE
+
+echo "Getting ArgoCD admin password..."
+ARGOCD_PASSWORD=$(kubectl -n $NAMESPACE get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+
+echo "Logging into ArgoCD..."
+argocd login argo-cd-argocd-server.$NAMESPACE.svc.cluster.local --username admin --password "$ARGOCD_PASSWORD" --insecure
+
+echo "Adding repository to ArgoCD..."
 argocd repo add https://github.com/quocanuit/just-test.git --type git
+
+echo "ArgoCD setup complete!"
